34feb5163f98c2ee0fd1123f996213a60ee8b112
Schafer Kristóf <54953202+csicso488@users.noreply.github.com>
2022-03-31 14:15:12 +0200

    script

---
+++ script.js
@@ -0,0 +1,17 @@
+let F = document.getElementById('fogyasztás').value;
+let V = document.getElementById('tank').value;
+let U = document.getElementById('ut').value;
+
+let megtehetoUt = V * 100 / F;
+
+if (megtehetoUt < Ut) {
+document.getElementById('megoldas').innerHTML = "Az út megtehető tankolás nélkül";
+} else {
+    document.getElementById('megoldas').innerHTML = "Az út megtehető tankolás nélkül"
+}
+
+
+
+
+
+
